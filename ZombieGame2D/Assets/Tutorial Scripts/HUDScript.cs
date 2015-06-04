f0e04e3894d4384e14041390dbda3b83ee2b55f8
jusrob2000 <jusrob2000@hotmail.com>
2015-06-04 15:39:43 -0700

    Added Exit button to Tutorial HUD

---
+++ ZombieGame2D/Assets/Tutorial Scripts/HUDScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUDScript : MonoBehaviour {
+
+	
+	public Button ExitBtn;
+	// Use this for initialization
+	void Start () {
+		ExitBtn = ExitBtn.GetComponent<Button>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	public void ExitPress()
+	{
+		Application.LoadLevel(0);
+	}
+}
