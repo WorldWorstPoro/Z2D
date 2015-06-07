38d24c30837cf6642aeeee47ff6a27eb45f1e327
jusrob2000 <jusrob2000@hotmail.com>
2015-06-07 14:15:37 -0700

    Added Ammo Notifier, Game/Gun balancing + more
    Biggest changes: Added Ammo section to the HUD to display current and
    max ammo capacity, added transition times to the zombie spawner so the
    game gets progressively more difficult, and tweaked the guns a little
    bit to give each its own advantages and disadvantages. Other smaller
    changes: Changed the shooting script from GetMouseButtonDown to
    GetMouseButton so holding the mouse now continually fires, making the
    machine gun a lot less tedious. Fixed the auto reloader when ammo is
    empty, before ammo would reach 0 but you would still have to click once
    more with zero ammo before the reload would happen, now it happens
    automatically right after it hits 0. Since that happens automatically
    now, I increased reload times a little bit to compensate for difficulty
    balancing. Also added relative reload rates for the manual reload, so if
    you had 3 of 6 bullets left and you manually reload it only takes half
    the normal time. This also fixes hitting reload with full ammo, before
    you'd still have to wait for a reload to complete before you could shoot
    again, even if you had full ammo.

--- ZombieGame2D/Assets/ShootBulletsScript.cs
+++ ZombieGame2D/Assets/ShootBulletsScript.cs
@@ -18,7 +18,7 @@ public class ShootBulletsScript : MonoBehaviour {
 	public Sprite m_shotgun;
 	public Sprite m_machinegun;
 
-	private int cur_bullets;
+	public int cur_bullets;
 	public int max_bullets;
 
 	private List<int> gunlist;
@@ -32,24 +32,21 @@ public class ShootBulletsScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (0) && Time.time > nextFire) {
-			if ( cur_bullets > 0 )
+		if (Time.time > nextFire && cur_bullets > 0) {
+			if (Input.GetMouseButton (0))
 			{
 				nextFire = Time.time + fireRate;
-				Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				Vector3 mouse_pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 				Vector2 unit_direction = new Vector2 ((mouse_pos.x - this.transform.position.x), (mouse_pos.y - this.transform.position.y));
-				unit_direction.Normalize();
+				unit_direction.Normalize ();
 				//Determines angle
 				float angle = Mathf.Atan2 (mouse_pos.y - this.transform.position.y, mouse_pos.x - this.transform.position.x) * Mathf.Rad2Deg;
 
-				if(cur_gun != 2)
-				{
+				if (cur_gun != 2) {
 					Rigidbody2D bullet_instance = Instantiate (m_bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, angle - 90))) as Rigidbody2D;
 					bullet_instance.velocity = (unit_direction * max_speed);
 					cur_bullets--;
-				}
-				else
-				{
+				} else {
 					Rigidbody2D bullet_instance1 = Instantiate (m_bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, angle - 90))) as Rigidbody2D;
 					Rigidbody2D bullet_instance2 = Instantiate (m_bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, angle - 90))) as Rigidbody2D;
 					Rigidbody2D bullet_instance3 = Instantiate (m_bullet, transform.position, Quaternion.Euler (new Vector3 (0, 0, angle - 90))) as Rigidbody2D;
@@ -61,29 +58,35 @@ public class ShootBulletsScript : MonoBehaviour {
 					Vector2 unit_direction4 = unit_direction;
 					Vector2 unit_direction5 = unit_direction;
 
-					unit_direction2 = Quaternion.Euler(0,0,5)*unit_direction2;
-					unit_direction3 = Quaternion.Euler(0,0,-5)*unit_direction3;
-					unit_direction4 = Quaternion.Euler(0,0,10)*unit_direction4;
-					unit_direction5 = Quaternion.Euler(0,0,-10)*unit_direction5;
-
+					unit_direction2 = Quaternion.Euler (0, 0, 5) * unit_direction2;
+					unit_direction3 = Quaternion.Euler (0, 0, -5) * unit_direction3;
+					unit_direction4 = Quaternion.Euler (0, 0, 10) * unit_direction4;
+					unit_direction5 = Quaternion.Euler (0, 0, -10) * unit_direction5;
+	
 					bullet_instance1.velocity = (unit_direction * max_speed);
 					bullet_instance2.velocity = (unit_direction2 * max_speed);
 					bullet_instance3.velocity = (unit_direction3 * max_speed);
 					bullet_instance4.velocity = (unit_direction4 * max_speed);
 					bullet_instance5.velocity = (unit_direction5 * max_speed);
 
-					
 					cur_bullets -= 5;
 				}
 			}
-			else if ( cur_bullets <= 0 )
-			{
-				nextFire = Time.time + reloadDelay;
-				cur_bullets = max_bullets;
-			}
+
+			ChangeReloadText(false);
 		}
-		if (Input.GetKeyDown (KeyCode.R)) {
+
+
+		if ( cur_bullets <= 0 ){
 			nextFire = Time.time + reloadDelay;
+			cur_bullets = max_bullets;
+			ChangeReloadText(true);
+		}
+
+		if (Input.GetKeyDown (KeyCode.R)) {
+			nextFire = Time.time + (reloadDelay * (1 - (cur_bullets * 1.0F / max_bullets * 1.0F)));
+			cur_bullets = max_bullets;
+			ChangeReloadText(true);
 		}
 		if (Input.GetKeyDown (KeyCode.Q)) {
 			if(gunlist.Count > 1)
@@ -124,30 +127,42 @@ public class ShootBulletsScript : MonoBehaviour {
 		if ( cur_gun == 0 )
 		{
 			gun_img.GetComponent<Image>().sprite = m_pistol;
-			fireRate = 0.3F;
-			reloadDelay = 0.4F;
+			fireRate = 0.4F;
+			reloadDelay = 0.8F;
 			max_bullets = 6;
 			cur_bullets = 6;
 			nextFire = Time.time + reloadDelay;
+			ChangeReloadText(true);
 		}
 		else if ( cur_gun == 1)
 		{
 			gun_img.GetComponent<Image>().sprite = m_machinegun;
 			fireRate = 0.1F;
-			reloadDelay = 0.8F;
+			reloadDelay = 1.7F;
 			max_bullets = 20;
 			cur_bullets = 20;
 			nextFire = Time.time + reloadDelay;
+			ChangeReloadText(true);
 		}
 		else if ( cur_gun == 2)
 		{
 			gun_img.GetComponent<Image>().sprite = m_shotgun;
-			fireRate = 0.7F;
-			reloadDelay = 1.5F;
+			fireRate = 0.9F;
+			reloadDelay = 1.2F;
 			max_bullets = 20;
 			cur_bullets	= 20;
 			nextFire = Time.time + reloadDelay;
+			ChangeReloadText(true);
 		}
 	}
 
+	public void ChangeReloadText(bool reloading)
+	{
+		GameObject reload_text = GameObject.Find ("AmmoText");
+
+		if (!reloading)
+			reload_text.GetComponent<Text> ().text = cur_bullets + "/" + max_bullets;
+		else
+			reload_text.GetComponent<Text> ().text = "Reloading...";
+	}
 }
--- ZombieGame2D/Assets/SpawnerScript.cs
+++ ZombieGame2D/Assets/SpawnerScript.cs
@@ -10,18 +10,32 @@ public class SpawnerScript : MonoBehaviour {
 	public GameObject SpawnerSE;
 	public Rigidbody2D m_zombie;
 
+	public int transition_one;
+	public int transition_two;
+	public int transition_three;
+	public int transition_four;
+	public int transition_five;
+
 	private int spawning;
+	private int timer;
+
+	public int spawn_rate;
+	public int zombie_speed;
 	// Use this for initialization
 	void Start () {
 		spawning = 0;
+		timer = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		spawning++;
+		timer++;
 
-		if (spawning == 100 && GameObject.Find ("Canvas").GetComponent<HUDScript>().EndText.GetComponent<GameOverScript>().dead != true) {
+		if (spawning >= spawn_rate && GameObject.Find ("Canvas").GetComponent<HUDScript>().EndText.GetComponent<GameOverScript>().dead != true) {
 			spawning = 0;
+
+			m_zombie.GetComponent<ZombieAvoidObstacleMovement>().max_speed = zombie_speed;
 
 			int rand = Random.Range(0,4);
 
@@ -40,8 +54,30 @@ public class SpawnerScript : MonoBehaviour {
 				Rigidbody2D zombie_instanceSE = Instantiate (m_zombie, SpawnerSE.transform.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
 				break;
 			default:
+				Rigidbody2D zombie_instanceNW2 = Instantiate (m_zombie, SpawnerNW.transform.position, Quaternion.Euler (new Vector3 (0, 0, 0))) as Rigidbody2D;
 				break;
 			}
+		}
+
+		if (timer == transition_one) {
+			spawn_rate = 100;
+		}
+
+		if (timer == transition_two) {
+			zombie_speed = 3;
+		}
+
+		if (timer == transition_three) {
+			spawn_rate = 50;
+		}
+
+		if (timer == transition_four) {
+			zombie_speed = 5;
+		}
+
+		if (timer == transition_five) {
+			spawn_rate = 40;
+			zombie_speed = 6;
 		}
 	}
 }
